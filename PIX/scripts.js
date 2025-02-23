c39e5f32f51cb6521704bee2aa32ddd612493994
RaphaelIniesta <raphael_iniesta@yahoo.com>
2025-02-23 20:27:25 -0300

    Concertar bugs

--- PIX/scripts.js
+++ PIX/scripts.js
@@ -17,3 +17,20 @@ function nota10() {
     document.getElementById('image').src = "https://www.conjur.com.br/img/b/emoji-joinha.jpeg"
     console.log("click")
 }
+
+function detectMobile() {
+    let device = '';
+ 
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)) {
+        device = true;
+    } else {
+        device = false;
+    }
+    console.log(device);
+}
